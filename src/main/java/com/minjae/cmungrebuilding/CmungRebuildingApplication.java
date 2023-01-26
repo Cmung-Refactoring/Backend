2b6be39ed3d253700a206eecce8ca93040de7a7f
Younseo Jang <younseojj@gmail.com>
2023-01-27 01:19:25 +0900

    initial commit

---
+++ src/main/java/com/minjae/cmungrebuilding/CmungRebuildingApplication.java
@@ -0,0 +1,13 @@
+package com.minjae.cmungrebuilding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmungRebuildingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmungRebuildingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minjae/cmungrebuilding/CmungRebuildingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minjae.cmungrebuilding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmungRebuildingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
