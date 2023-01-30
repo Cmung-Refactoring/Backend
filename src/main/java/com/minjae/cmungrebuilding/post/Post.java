9e7ffc3d056ccbdc3d3a8386ef5391bb823efdc1
Younseo Jang <younseojj@gmail.com>
2023-01-30 19:12:09 +0900

    Fix : Post Entity (table join)
    Co-authored-by: Woozy <anfrosus@naver.com>
    Co-authored-by: meruberu <wmj0924@gmail.com>

--- src/main/java/com/minjae/cmungrebuilding/post/Post.java
+++ src/main/java/com/minjae/cmungrebuilding/post/Post.java
@@ -1,6 +1,7 @@
-package com.minjae.cmungrebuilding.entity;
+package com.minjae.cmungrebuilding.post;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.minjae.cmungrebuilding.member.Member;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -31,10 +32,8 @@ public class Post {
     @Column(nullable = false)
     private String local;
 
-    @JsonBackReference
-    @ManyToOne
-    @JoinColumn(name = "categoryId", nullable = false) // 카테고리를 테이블 분리해서 관리해야 할까?
-    private Category category;
+    @Column(nullable = false)
+    private String category;
 
     @JsonBackReference
     @ManyToOne
@@ -42,8 +41,8 @@ public class Post {
     private Member member;
     // nickname을 원래 따로 컬럼값으로 가지고 있었는데, member를 테이블 조인하면 언제든 원할 때 사용할 수 있으니 nickname column은 제외했습니다.
 
-    @JsonBackReference
-    @OneToMany(mappedBy = "post", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
-    private List<Image> image;
+//    @JsonBackReference
+//    @OneToMany(mappedBy = "post", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+//    private List<Image> image;
 
 }
