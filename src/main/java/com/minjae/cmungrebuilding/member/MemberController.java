454324897b6f055f641db4973ba0a23674ffe633
Younseo Jang <younseojj@gmail.com>
2023-02-01 21:09:26 +0900

    Feat : memberController
    Co-authored-by: meruberu <wmj0924@gmail.com>

--- src/main/java/com/minjae/cmungrebuilding/member/MemberController.java
+++ src/main/java/com/minjae/cmungrebuilding/member/MemberController.java
@@ -1,12 +1,11 @@
 package com.minjae.cmungrebuilding.member;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.minjae.cmungrebuilding.global.GlobalResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @RestController  // -> @Controller + @ResponseBody
@@ -15,6 +14,7 @@ import javax.validation.Valid;
 public class MemberController {
 
     private final MemberService memberService;
+    private final SocialService socialService;
 
     // 일반 회원가입
     @PostMapping("/signup")
@@ -23,20 +23,21 @@ public class MemberController {
     }
 
     // 카카오 회원가입
-//    @GetMapping("/kakao/callback")
-//    public  GlobalResponseDto<?> kakaoLogin(@RequestParam String code,
-//                                            HttpServletResponse response) throws JsonProcessingException {
-//        return memberService.kakaoLogin(code, response);
-//    }
-//
-//    // 네이버 회원가입
-//    @GetMapping("/naver/callback")
-//    public GlobalResponseDto<?> naverLogin(@RequestParam code, @RequestParam String state,
-//                                           HttpServletResponse response){
-//
-//    }
-//
-//
+    @GetMapping("/kakao/callback")
+    public  GlobalResponseDto<?> kakaoLogin(@RequestParam String code,
+                                            HttpServletResponse response) throws JsonProcessingException {
+        return socialService.kakaoLogin(code, response);
+    }
+
+    // 네이버 회원가입
+    @GetMapping("/naver/callback")
+    public GlobalResponseDto<?> naverLogin(@RequestParam String code,
+                                           @RequestParam String state,
+                                           HttpServletResponse response){
+        return socialService.naverLogin(code, state, response);
+    }
+
+
     // 아이디 중복확인
     @PostMapping("/idcheck")
     public GlobalResponseDto<?> emailCheck (@RequestBody MemberRequestDto memberRequestDto) {
@@ -50,4 +51,8 @@ public class MemberController {
     }
 
     // 로그인
+
+    // 로그아웃
+
+    // 회원 탈퇴 기능
 }
